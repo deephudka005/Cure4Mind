1d3be4ceb169cc806d61cc3e3b51b33669697a56
Deep Hudka <deep.hudka.19041@iitgoa.ac.in>
2022-02-13 11:47:14 +0530

    ...

--- app/src/main/java/com/cureya/cure4mind/MainActivity.kt
+++ app/src/main/java/com/cureya/cure4mind/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.cureya.cure4mind
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(com.cureya.cure4mind.R.layout.activity_main)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+
+        val bottomNavView = findViewById<BottomNavigationView>(com.cureya.cure4mind.R.id.nav_view)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(com.cureya.cure4mind.R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        bottomNavView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(com.cureya.cure4mind.R.menu.top_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            com.cureya.cure4mind.R.id.aboutUsfragment -> {
+                navController.navigate(com.cureya.cure4mind.R.id.aboutUsfragment)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return super.onSupportNavigateUp() || navController.navigateUp()
     }
 }
---
+++ app/src/main/java/com/cureya/cure4mind/home/AboutUsFragment.kt
@@ -0,0 +1,27 @@
+package com.cureya.cure4mind
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.cureya.cure4mind.databinding.FragmentAboutUsBinding
+
+
+class AboutUsFragment : Fragment() {
+    private lateinit var binding: FragmentAboutUsBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentAboutUsBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
+}
